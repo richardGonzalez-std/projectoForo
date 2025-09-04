fdc7459e2ff1a89e98d3bc8b59494e800166a543
Richard Gonzalez <zambranoa998@gmail.com>
2025-09-04 17:55:11 -0400

    prueba inline script

--- foro.php
+++ foro.php
@@ -107,6 +107,10 @@
         ?>
         </table>
 
-        <script src="script.js"></script>
+        <script>
+            document.querySelector('#light_mode').addEventListener('click',function(){
+                console.log("this is working");
+            });
+        </script>
     </body>
 </html>
