0e8065b32d2e007450c3d78413a0996bd5ee7ac4
Richard Gonzalez <zambranoa998@gmail.com>
2025-09-04 18:55:16 -0400

    darkmode;

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(){
     document.querySelector('#light_mode').addEventListener('click',function(){
         this.textContent = "Dark Mode";
         this.className.add("dark-mode");
-        document.body.style.background="black";
+        document.body.style.backgroundColor="black";
         document.body.style.color="white";
     });
 })
