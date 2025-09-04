36af7968df0a89a5f0cc812b5193935072703f79
Richard Gonzalez <zambranoa998@gmail.com>
2025-09-04 17:35:35 -0400

    Cambio darkMode

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.querySelector('#light_mode').addEventListener('click',function(){
-    document.body.style.background="black";
-    document.body.style.color="white";
-    document.querySelector("input").style.background="#d6ecc16e";
+    document.querySelector("body").style.background="black";
+    this.textContent="Dark Mode";
+    this.id="dark_mode";
 })
