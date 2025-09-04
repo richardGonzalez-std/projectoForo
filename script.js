7a8b087ae7b7cc7d5e5fd82b997983fb8e48e29b
Richard Gonzalez <zambranoa998@gmail.com>
2025-09-04 19:07:39 -0400

    cambio

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(){
     document.querySelector('#light_mode').addEventListener('click',function(){
         this.textContent = "Dark Mode";
-        this.className.add("dark-mode");
-        document.body.style.backgroundColor="black";
-        document.body.style.color="white";
-        document.querySelector("input").style.background="#d6ecc16e";
+        this.id = "dark_mode";
+        document.querySelector("body").style.backgroundColor="black";
+        document.querySelector("body").style.color="white";
     });
 })
