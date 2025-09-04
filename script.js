e811bdabc9d9467838d7c4fb1143d2ddb28c74fd
Richard Gonzalez <zambranoa998@gmail.com>
2025-09-04 19:54:27 -0400

    cambio modes

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(){
-    var light_mode = document.querySelector('#light_mode').textContent;
-    var dark_mode = document.querySelector('#light_mode').textContent;
-    if(light_mode == "Light Mode"){
+    var mode = document.querySelector('button').textContent;
+    if(mode != "Light Mode"){
         document.querySelector('#dark_mode').addEventListener('click',function(){
         this.textContent = "Light Mode";
         this.id = "light_mode";
         document.querySelector("body").style.backgroundColor="white";
         document.querySelector("body").style.color="black";
     });
-    }else if(dark_mode == "Dark Mode"){
+    }else if(mode != "Dark Mode"){
     document.querySelector('#light_mode').addEventListener('click',function(){
         this.textContent = "Dark Mode";
         this.id = "dark_mode";
