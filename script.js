c9740cdcf46aa68ebe1540f3532cb330bbce62dc
Richard Gonzalez <zambranoa998@gmail.com>
2025-09-04 20:19:17 -0400

    cambio css

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const btn = document.querySelector('#light_mode');
+    const btn = document.querySelector('button');
 
     btn.addEventListener('click', function () {
-        document.body.classList.toggle("light-mode");
-
-        if (document.body.classList.contains("light-mode")) {
-            btn.textContent = "Dark Mode";
-        } else {
-            btn.textContent = "Light Mode";
+        if(btn.classList.contains("light_mode")){
+            btn.classList.remove("light_mode");
+            btn.classList.add("dark_mode");
+        }else if(btn.classList.contains("dark_mode")){
+            btn.classList.remove("dark_mode");
+            btn.classList.add("light_mode");
         }
     });
 });
